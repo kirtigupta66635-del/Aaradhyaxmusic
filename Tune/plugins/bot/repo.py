bef9c341a79321c37c3c7aac896ac12bd22f8d66
VAMPIRE BOTS <vampirebots27@gmail.com>
2025-07-31 20:07:34 +0530

    Update repo.py

--- Tune/plugins/bot/repo.py
+++ Tune/plugins/bot/repo.py
@@ -4,7 +4,7 @@ from Tune import app
 from config import BOT_USERNAME
 
 repo_caption = """**
-🚀 ᴄʟᴏɴᴇ ᴀɴᴅ ᴅᴇᴘʟᴏʏ – ᴄᴇʀᴛɪꜰɪᴇᴅ ᴄᴏᴅᴇʀꜱ ʀᴇᴘᴏ 🚀
+🚀 ᴄʟᴏɴᴇ ᴀɴᴅ ᴅᴇᴘʟᴏʏ – ᴠᴀᴍᴘɪʀᴇ ᴄᴏᴅᴇʀꜱ ʀᴇᴘᴏ 🚀
 
 ➤ ᴅᴇᴘʟᴏʏ ᴇᴀsɪʟʏ ᴏɴ ʜᴇʀᴏᴋᴜ ᴡɪᴛʜᴏᴜᴛ ᴇʀʀᴏʀꜱ  
 ➤ ɴᴏ ʜᴇʀᴏᴋᴜ ʙᴀɴ ɪꜱꜱᴜᴇ  
@@ -20,19 +20,19 @@ async def show_repo(_, msg):
     buttons = [
         [InlineKeyboardButton("➕ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ✨", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
         [
-            InlineKeyboardButton("👑 ᴏᴡɴᴇʀ", url="https://t.me/CertifiedCoder"),
-            InlineKeyboardButton("💬 ꜱᴜᴘᴘᴏʀᴛ", url="https://t.me/CertifiedCodes")
-        ],
+            InlineKeyboardButton("👑 ᴏᴡɴᴇʀ", url="https://t.me/llVAMPIRE_KINGlll"),
+            InlineKeyboardButton("💬 ꜱᴜᴘᴘᴏʀᴛ", url="https://t.me/lllVAMPIRE_UPDATElll")
+        ],h
         [
-            InlineKeyboardButton("🛠️ ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url="https://t.me/CertifiedCoders"),
-            InlineKeyboardButton("🎵 ɢɪᴛʜᴜʙ", url="https://github.com/CertifiedCoders/TuneViaBot")
+            InlineKeyboardButton("🛠️ ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url="https://t.me/lllVAMPIRE_UPDATElll"),
+            InlineKeyboardButton("🎵 ɢɪᴛʜᴜʙ", url="https://files.catbox.moe/of4rtv.jpg")
         ]
     ]
 
     reply_markup = InlineKeyboardMarkup(buttons)
 
     await msg.reply_photo(
-        photo="https://files.catbox.moe/lp5sel.jpg",
+        photo="https://files.catbox.moe/kbi6t5.jpg",
         caption=repo_caption,
         reply_markup=reply_markup
     )
