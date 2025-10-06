1a19235512a351d07acaf1c2f5406526b90b59e3
kirtigupta66635-del <kirtigupta66635@gmail.com>
2025-10-06 14:18:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,8 +32,8 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "5242880000"))
 
 # ───── Custom API Configs ───── #
 COOKIE_URL = getenv("COOKIE_URL") #necessary
-API_URL = getenv("API_URL") #optional
-API_KEY = getenv("API_KEY") #optional
+API_URL = getenv("https://www.googleapis.com/youtube/v3") #optional
+API_KEY = getenv("AAIzaSyB2zalmv7EoEbRpZANqqYLoqzJ2RPdXKW8") #optional
 
 # ───── Heroku Configuration ───── #
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
